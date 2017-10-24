4896bedd99a36ae627af4bf24a5303192cfc60e3
Nenad Kuzmanovic <kuzma86@gmail.com>
2017-10-25 01:14:26 +0200

    Simple search mesure time

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,14 @@ let simpleSearch = new SimpleSearch(friends);
 
 // simple search
 document.getElementById('simple-search').addEventListener('click', () => {
-
      // Get connections of first friend
+     let start = new Date().getTime();
      let connections = simpleSearch.getConnections("Jaclyn");
+     let end = new Date().getTime();
+
+     document.getElementById('simple-search-time')
+        .innerHTML = ((end - start) / 1000).toString();
+
      console.log(connections);
+
 })
